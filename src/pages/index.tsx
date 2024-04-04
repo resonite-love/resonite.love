fcf93a6ad11ab2c1bc34271bb473d9ef918104a7
rassi0429 <taitokokoa+rassi@gmail.com>
2024-04-04 10:53:48 +0900

    chore: chore

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+function IndexPage(){
+  return (
+    <div>
+      <h1>Resonite💛</h1>
+    </div>
+  );
+}
+
+export default IndexPage;
---
+++ src/pages/test.tsx
@@ -0,0 +1,11 @@
+
+
+function Test() {
+  return (
+    <div>
+      <div>Test</div>
+    </div>
+  );
+}
+
+export default Test;
