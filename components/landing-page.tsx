93221d3ff656709842c437c11781f595d7c711db
ぞぞかす <jojohounds@gmail.com>
2024-11-20 04:36:35 +0900

    add link to jp guide
    非公式日本語ガイド

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -62,7 +62,7 @@ export function LandingPageComponent() {
 
   const resources = [
     { title: "公式Wiki", url: "https://wiki.resonite.com", description: "Resoniteの公式Wikiサイトです。", color: "from-red-500 to-orange-500" },
-    { title: "ぞぞかす非公式Wiki", url: "#", description: "ユーザーによって作成・管理されているWikiです。", color: "from-orange-500 to-yellow-500" },
+    { title: "Resonite 非公式日本語ガイド", url: "https://resonite-jp-guide.zozoka.su/", description: "Resoniteにある機能を一通り解説した日本語ガイドサイトです。個人によって管理・運営されています。", color: "from-orange-500 to-yellow-500" },
     { title: "べるねWiki", url: "#", description: "ユーザーによって作成・管理されているWikiです。", color: "from-orange-500 to-yellow-500" },
     { title: "初心者ガイド", url: "#", description: "Resoniteを始めたばかりの方向けの解説サイトです。", color: "from-yellow-500 to-green-500" },
     { title: "初心者ガイド", url: "#", description: "Resoniteを始めたばかりの方向けの解説サイトです。", color: "from-yellow-500 to-green-500" },
